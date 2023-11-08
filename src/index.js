befc13bc83b7d6a7a07380b60941d9aec9160878
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2023-11-08 22:13:08 +0100

    addet animation

--- src/index.js
+++ src/index.js
@@ -17,3 +17,23 @@ document.getElementById("openNav").addEventListener("click", () => {
   })
 
   document.getElementById("copyright").innerHTML = "&copy; " + new Date().getFullYear() + " Argo-Advisory";
+
+
+
+ // observer 
+
+ const serviceCards = document.querySelectorAll(".service-card");
+
+const intersectionObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const card = entry.target;
+      card.classList.add('animate__animated', 'animate__fadeInRight');
+    }
+  });
+});
+
+// Передайте каждый элемент с классом "service-card" в качестве целевых элементов IntersectionObserver
+serviceCards.forEach((card) => {
+  intersectionObserver.observe(card);
+});
