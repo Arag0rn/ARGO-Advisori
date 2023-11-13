9ba94f8f7a18ff9ede47ba3cf74a4f360184dd1b
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2023-11-13 19:18:01 +0100

    add lang switch

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
+
+import {transl} from './translation.js';
+
+
+
+
 // gamburger
 
-document.getElementById("openNav").addEventListener("click", () => {
-    document.getElementById("mainSideNav").style.width = "100%";
-  })
+// document.getElementById("openNav").addEventListener("click", () => {
+//     document.getElementById("mainSideNav").style.width = "100%";
+//   })
 
-  document.getElementById("closeNav").addEventListener("click", () => {
-    document.getElementById("mainSideNav").style.width = "0";
-  })
+//   document.getElementById("closeNav").addEventListener("click", () => {
+//     document.getElementById("mainSideNav").style.width = "0";
+//   })
 
-  document.getElementById("navbar-link-about").addEventListener("click", () => {
-    document.getElementById("mainSideNav").style.width = "0";
-  })
+//   document.getElementById("navbar-link-about").addEventListener("click", () => {
+//     document.getElementById("mainSideNav").style.width = "0";
+//   })
 
-  document.getElementById("navbar-link-services").addEventListener("click", () => {
-    document.getElementById("mainSideNav").style.width = "0";
-  })
+//   document.getElementById("navbar-link-services").addEventListener("click", () => {
+//     document.getElementById("mainSideNav").style.width = "0";
+//   })
 
-  document.getElementById("copyright").innerHTML = "&copy; " + new Date().getFullYear() + " Argo-Advisory";
+//   document.getElementById("copyright").innerHTML = "&copy; " + new Date().getFullYear() + " Argo-Advisory";
 
 
 
@@ -37,3 +43,37 @@ const intersectionObserver = new IntersectionObserver((entries) => {
 serviceCards.forEach((card) => {
   intersectionObserver.observe(card);
 });
+
+//langswitcher
+
+const langSel = document.querySelector('select');
+const allLang = ['en', 'de'];
+
+
+langSel.addEventListener('change', onChangeUrlLanguage);
+
+function onChangeUrlLanguage() {
+  let lang = langSel.value;
+  location.href = window.location.pathname + '#' + lang;
+  location.reload();
+}
+
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substring(1);
+  console.log(hash);
+  if (!allLang.includes(hash)){
+    location.href = window.location.pathname + '#en';
+    location.reload();
+  }
+  langSel.value = hash;
+  for (let key in transl){
+    let elem = document.querySelector('.lng-' + key);
+    if(elem) {
+    elem.textContent = transl[key][hash]
+  }
+  }
+}
+
+changeLanguage ()
+
---
+++ src/translation.js
@@ -0,0 +1,58 @@
+export const transl = {
+  // OUR FOCUS
+  "our-focus-name": {
+    "en": "OUR FOCUS",
+    "de": "UNSER FOKUS",
+  },
+
+  // our service
+  "card-head1": {
+      "en": "Business Valuation",
+      "de": "Unternehmensbewertung",
+  },
+  "card-sub1": {
+    "en": "Know where you are",
+    "de": "Wissen Sie, wo Sie sind",
+  },
+  "card-head2": {
+    "en": "Due Diligence",
+    "de": "Willkommen auf unserer Website",
+  },
+  "card-sub2": {
+    "en": "Welcome to our website",
+    "de": "Willkommen auf unserer Website",
+  }, 
+  "card-head3": {
+    "en": "Welcome to our website",
+    "de": "Willkommen auf unserer Website",
+  },
+  "card-sub3": {
+    "en": "Welcome to our website",
+    "de": "Willkommen auf unserer Website",
+  },
+  "card-head4": {
+  "en": "Welcome to our website",
+  "de": "Willkommen auf unserer Website",
+  },
+  "card-sub4": {
+    "en": "Welcome to our website",
+    "de": "Willkommen auf unserer Website",
+  },
+  "card-head5": {
+    "en": "Welcome to our website",
+    "de": "Willkommen auf unserer Website",
+  },
+  "card-sub5": {
+    "en": "Welcome to our website",
+    "de": "Willkommen auf unserer Website",
+  },
+  "card-head6": {
+  "en": "Welcome to our website",
+  "de": "Willkommen auf unserer Website",
+  },
+  "card-sub6": {
+    "en": "Welcome to our website",
+    "de": "Willkommen auf unserer Website",
+  },
+  
+};
