4e3c20daa0847ff3c6cc1df45682475014f159d3
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2023-12-19 09:25:45 +0000

    Deploying to gh-pages from @ Arag0rn/ARGO-Advisori@8cfcce007dfc4b40b24c2f5e60927983a53731a0 🚀

---
+++ partials/insihts-and-resourses.f9eeec92.js
@@ -0,0 +1,2 @@
+document.querySelector(".resourses-list-left-btn").addEventListener("click",(function(e){e.preventDefault();window.location.href="./index.html#getInTouch"}));
+//# sourceMappingURL=insihts-and-resourses.f9eeec92.js.map
