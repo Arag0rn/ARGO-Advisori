6f8d64f558d138be7788407f4664ac7a023c960f
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-01-04 14:55:28 +0000

    Deploying to gh-pages from @ Arag0rn/ARGO-Advisori@a21a1755f4b87abd6c8226a2ede32998b9534275 🚀

--- partials/insihts-and-resourses.f0dc9590.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".resourses-list-left-btn").addEventListener("click",(function(e){e.preventDefault(),window.location.href="./index.html#getInTouch"}));
-//# sourceMappingURL=insihts-and-resourses.f0dc9590.js.map
--- partials/insihts-and-resourses.f9eeec92.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".resourses-list-left-btn").addEventListener("click",(function(e){e.preventDefault();window.location.href="./index.html#getInTouch"}));
-//# sourceMappingURL=insihts-and-resourses.f9eeec92.js.map
