5f4a4ab0aac8801b8038dfdd4833597f852e3b5a
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-01-05 12:32:37 +0100

    finished tablet

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -8,5 +8,16 @@ const swiper = new Swiper(".swiper", {
     navigation: {
       nextEl: ".button-next",
       prevEl: ".button-prev"
+    },
+    breakpoints: {
+      836: {
+        slidesPerView: 2,
+        spaceBetween: 37
+      },
+      1444: {
+        slidesPerView: 3.6,
+        spaceBetween: 37
+      },
+
     }
   });
