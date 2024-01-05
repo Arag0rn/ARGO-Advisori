19c9503a7e9ea7c90e16a31ac3ec5726d154488f
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-01-05 12:56:31 +0100

    Update scrolTo.js

--- src/js/scrolTo.js
+++ src/js/scrolTo.js
@@ -1,24 +1,22 @@
 function scrollToSection(event) {
   event.preventDefault();
-  const targetHref = "index.html";
+  const targetHref = event.target.getAttribute('href'); // Получаем значение атрибута href
   const targetElement = document.querySelector(targetHref);
 
   if (targetElement) {
-      window.scrollTo({
-          top: targetElement.offsetTop,
-          behavior: 'smooth'
-      });
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: 'smooth'
+    });
   }
 }
 
-  const journeyBtn = document.querySelector(".journey-btn");
+const journeyBtn = document.querySelector(".journey-btn");
 
-  journeyBtn.addEventListener("click", scrollToSection);
+journeyBtn.addEventListener("click", scrollToSection);
 
-  const links = document.querySelectorAll('.header-link');
+const links = document.querySelectorAll('.header-link');
 
-  links.forEach(link => {
-        link.addEventListener('click', scrollToSection);
-    });
-
-
+links.forEach(link => {
+  link.addEventListener('click', scrollToSection);
+});
