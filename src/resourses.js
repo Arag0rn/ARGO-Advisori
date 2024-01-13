b0fbc13c97dad6dfd955f627c38a7457330f29d6
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-01-13 12:57:25 +0100

    add second article

--- src/resourses.js
+++ src/resourses.js
@@ -3,27 +3,28 @@ const resoursesBtn = document.querySelector(".resourses-list-left-btn");
 resoursesBtn.addEventListener("click", function(event) {
   event.preventDefault();
 
-  const targetSectionId = "getInTouch"; 
+  const targetSectionId = "getInTouch";
 
   window.location.href = "./index.html#" + targetSectionId;
 });
 
 document.addEventListener('DOMContentLoaded', function () {
   const readMoreBoxes = document.querySelectorAll('.resourses-readMore-box');
-  const articlesList = document.querySelector('.articles-list');
+  const articlesList = document.querySelectorAll('.articles-list-item');
 
 
   readMoreBoxes.forEach(box => {
       box.addEventListener('click', function () {
           const articleId = this.closest('.resoursed-card').dataset.articleId;
 
-          const articleElement = articlesList.querySelector(`#article-${articleId}`);
+          const articleElement = document.querySelector(`#article-${articleId}`);
 
           if (articleElement) {
-              document.querySelector('.resourses-list').classList.add('visually-hidden', 'animate__animated', 'animate__slideInLeft', 'animate__medium');
-              document.querySelector('.resourses-list-head').classList.add('visually-hidden', 'animate__animated', 'animate__slideInLeft', 'animate__medium');
+              document.querySelector('.resourses-list').classList.add('visually-hidden');
+              document.querySelector('.resourses-list-head').classList.add('visually-hidden');
 
-              articlesList.classList.remove('visually-hidden', 'animate__animated', 'animate__slideInLeft', 'animate__medium');
+              articleElement.classList.remove('visually-hidden');
+              articleElement.classList.add('animate__animated', 'animate__fadeIn', 'animate__medium');
 
               articleElement.scrollIntoView({ behavior: 'smooth' });
           }
