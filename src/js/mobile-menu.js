1ae1946d4f301a652f9c59379f50499002af0198
Serhii Khabuzov <habuzovs@gmail.com>
2024-02-04 17:00:43 +0100

    fixed scroll

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,47 +1,47 @@
-const mobileMenu = document.querySelector('.js-menu-container');
 const openMenuBtn = document.querySelectorAll('.js-open-menu');
 const closeMenuBtn = document.querySelectorAll('.js-close-menu');
-console.log(openMenuBtn);
+const mobileMenu = document.querySelector('.js-menu-container');
 
 
 const toggleMenu = (btn) => {
-  console.log(btn);
-
   const isMenuOpen =
   btn.getAttribute('aria-expanded') === 'true' || false;
   btn.setAttribute('aria-expanded', !isMenuOpen);
   mobileMenu.classList.toggle('is-open');
 
-  if (isMenuOpen) {
+  if (isMenuOpen()) {
     document.body.style.overflow = 'auto';
   } else {
-
     document.body.style.overflow = 'hidden';
   }
+
+  const scrollLockMethod = !isMenuOpen
+  ? 'disableBodyScroll'
+  : 'enableBodyScroll';
+bodyScrollLock[scrollLockMethod](document.body);
 };
 
-// const scrollToSection = (sectionId) => {
-//   const section = document.querySelector(sectionId);
-//   if (section) {
-//     section.scrollIntoView({ behavior: 'smooth' });
-//     toggleMenu();
-//   }
-// };
+
+const scrollToSection = (sectionId) => {
+  const section = document.querySelector(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+    toggleMenu(openMenuBtn[0]);
+  }
+
+};
+
 
 
 openMenuBtn.forEach((btn) => {
-btn.addEventListener('click', ()=> toggleMenu(btn));
-})
-
-
+  btn.addEventListener('click', () => toggleMenu(btn));
+});
 
 closeMenuBtn.forEach(btn => {
-  btn.addEventListener('click', ()=> toggleMenu(btn));
-})
+  btn.addEventListener('click', () => toggleMenu(btn));
+});
 
-
-
-document.querySelectorAll('.toggleMenu-item a').forEach((link) => {
+document.querySelectorAll('.header-toggleMenu-item a').forEach((link) => {
   link.addEventListener('click', (event) => {
     event.preventDefault();
     const sectionId = link.getAttribute('href');
@@ -49,9 +49,9 @@ document.querySelectorAll('.toggleMenu-item a').forEach((link) => {
   });
 });
 
-window.matchMedia('(min-width: 768px)').addEventListener('change', (e) => {
+window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
   if (!e.matches) return;
   mobileMenu.classList.remove('is-open');
   openMenuBtn.setAttribute('aria-expanded', false);
-  document.body.style.overflow = 'auto'; // Enable body scroll
+  bodyScrollLock.enableBodyScroll(document.body);
 });
