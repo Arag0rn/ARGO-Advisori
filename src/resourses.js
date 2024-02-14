b58909ee74917695edde319743df5673a7f71980
Serhii Khabuzov <habuzovs@gmail.com>
2024-02-14 09:36:48 +0100

    added JSON articles

--- src/resourses.js
+++ src/resourses.js
@@ -1,4 +1,48 @@
-document.addEventListener('DOMContentLoaded', function () {
+import data from '../articles.json';
+
+console.log(data);
+
+
+function createArticleHTML(data) {
+    const articleElement = document.createElement('li');
+    articleElement.id = data.id;
+    articleElement.className = data.class;
+  
+    const titleElement = document.createElement('h3');
+    titleElement.className = data.content[0].class; 
+    titleElement.textContent = data.title;
+  
+    articleElement.appendChild(titleElement);
+  
+
+    for (let i = 1; i < data.content.length; i++) {
+      const contentItem = data.content[i];
+      const contentElement = document.createElement('p');
+      contentElement.className = contentItem.class;
+      contentElement.textContent = contentItem.text;
+  
+      articleElement.appendChild(contentElement);
+    }
+  
+
+    const linkElement = document.createElement('a');
+    linkElement.href = "https://argo-advisory.com/insihts-and-resourses.html"; 
+    const imgElement = document.createElement('img');
+    imgElement.className = 'service-arrow resourses-service-arrow back-btn';
+    imgElement.alt = 'arrow';
+    const cloudinaryImageUrl ="https://res.cloudinary.com/drkjdderc/image/upload/v1707576249/left-arrow_vpjg8g.png"
+    imgElement.src = cloudinaryImageUrl;
+
+    linkElement.appendChild(imgElement);
+    console.log('Cloudinary Image URL:', cloudinaryImageUrl);
+
+    articleElement.appendChild(linkElement);
+
+    return articleElement;
+  }
+
+
+function setupEventListeners() {
   const readMoreBoxes = document.querySelectorAll('.resourses-readMore-box');
   const backButtons = document.querySelectorAll('.back-btn-box');
 
@@ -10,35 +54,48 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
+  
+
+
   readMoreBoxes.forEach(box => {
     box.addEventListener('click', function () {
       const articleId = this.closest('.resoursed-card').dataset.articleId;
-      const articleElement = document.querySelector(`#article-${articleId}`);
- 
 
-      if (articleElement) {
+      const article = data.articles.find(article => article.id === `article-${articleId}`);
+
+      if (article) {
         document.querySelector('.resourses-list').classList.add('visually-hidden');
         document.querySelector('.resourses-list-head').classList.add('visually-hidden');
-
-        articleElement.classList.remove('visually-hidden');
-        articleElement.classList.add('animate__animated', 'animate__fadeIn', 'animate__medium');
-
-        articleElement.scrollIntoView({ behavior: 'smooth' });
+      
+        const articlesList = document.querySelector('.articles-list');
+        const newArticle = createArticleHTML(article);
+        articlesList.appendChild(newArticle);
+      
+        newArticle.classList.add('animate__animated', 'animate__fadeIn', 'animate__medium');
+        newArticle.scrollIntoView({ behavior: 'smooth' });
       }
     });
   });
+}
 
-  const urlParams = new URLSearchParams(window.location.search);
-  const articleId = urlParams.get('article');
+document.addEventListener('DOMContentLoaded', setupEventListeners);
 
-  if (articleId) {
-    const articleElement = document.querySelector(`#article-${articleId}`);
-    if (articleElement) {
-      articleElement.classList.remove('visually-hidden');
-      articleElement.classList.add('animate__animated', 'animate__fadeIn', 'animate__medium');
-      document.querySelector('.resourses-list').classList.add('visually-hidden');
-      document.querySelector('.resourses-list-head').classList.add('visually-hidden');
-      articleElement.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
-});
+
+const urlParams = new URLSearchParams(window.location.search);
+const articleId = urlParams.get('article');
+const article = data.articles.find(article => article.id === `article-${articleId}`);
+console.log(article);
+if (article) {
+  document.querySelector('.resourses-list').classList.add('visually-hidden');
+  document.querySelector('.resourses-list-head').classList.add('visually-hidden');
+
+  const articlesList = document.querySelector('.articles-list');
+  const newArticle = createArticleHTML(article);
+  articlesList.appendChild(newArticle);
+
+
+  newArticle.classList.add('animate__animated', 'animate__fadeIn', 'animate__medium');
+  newArticle.scrollIntoView({ behavior: 'smooth' });
+}
+
+
